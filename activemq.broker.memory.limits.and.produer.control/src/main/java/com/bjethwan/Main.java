6560029a1f925be545201a352f11905c481c932b
Bipin Jethwani <jethwani.bipin@gmail.com>
2017-05-05 09:24:09 +0530

    connection pooling and producer flow control

---
+++ activemq.broker.memory.limits.and.produer.control/src/main/java/com/bjethwan/Main.java
@@ -0,0 +1,106 @@
+package com.bjethwan;
+
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.pool.PooledConnectionFactory;
+
+public class Main {
+
+	private final String connectionUri = "vm://bipin-emm?create=false";
+	private final String brokerConfig = "xbean:emm_activemq.xml";
+
+	private BrokerService broker;
+
+	private ActiveMQConnectionFactory connectionFactory;
+	private PooledConnectionFactory pooledFactory;
+
+	private final int NUM_DEVICES = 40;
+	private final int TOMCAT_THREADS = 20;
+
+	private CountDownLatch done;
+
+	public void before() throws Exception{
+
+		startEmbeddedBrokerWithGivenConfig(brokerConfig);
+
+		connectionFactory = new ActiveMQConnectionFactory(connectionUri);
+		pooledFactory = new PooledConnectionFactory(connectionFactory);
+	}
+
+	public void runPerformanceTest() throws Exception{
+		//This should behave like tomcat threads
+		
+		ExecutorService service = Executors.newFixedThreadPool(TOMCAT_THREADS);
+		
+		int count = 100;
+		while((count--)>0){
+			
+			this.done = new CountDownLatch(NUM_DEVICES);
+			
+			for(int i=1; i<=NUM_DEVICES; i++){
+				service.execute(new Runnable(){
+					@Override
+					public void run() {
+						try {
+							Connection connection = pooledFactory.createConnection();
+							Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+							Queue queue = session.createQueue("hrtBeatQueue");
+							MessageProducer producer = session.createProducer(queue);
+							producer.send(session.createTextMessage("Biggggggggggggggggggggggggggggggggggg gggggggggggggg ggggggggggggg gggggggggg Test Message"));
+							producer.close();
+							session.close();
+							connection.close();
+						} catch (JMSException e) {
+							e.printStackTrace();
+						} finally {
+							done.countDown();
+						}
+					}
+				});
+			}
+			
+			done.await();
+			TimeUnit.SECONDS.sleep(2);
+		}
+		
+		service.shutdown();
+
+	}
+
+	public void after() throws Exception {
+		pooledFactory.clear();
+		broker.stop();
+	}
+
+	public void startEmbeddedBrokerWithGivenConfig(String config) throws Exception{
+
+		broker = BrokerFactory.createBroker(new URI(config));
+		broker.start();
+	}
+
+
+
+	public static void main(String[] args) throws Exception {
+
+		Main sample = new Main();
+
+		sample.before();
+		sample.runPerformanceTest();
+		//sample.after();
+
+	}
+}
